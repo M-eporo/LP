4bec00700abce6da54e36b6b879ababeba4e9787
M-eporo <gshuji1021@gmail.com>
2026-02-08 10:17:36 +0900

    comfirm

--- js/gsap-core.js
+++ js/gsap-core.js
@@ -1,20 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-    gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-
+    gsap.registerPlugin(ScrollTrigger, MotionPathPlugin);
     // 1文字ずつspan化 - <br>保持
     const wrapChars = (el) => {
         if (!el) return [];
-
         // すでに処理済みなら二重にしない
         if (el.dataset.charsWrapped === "1") {
             return Array.from(el.querySelectorAll(".char"));
         }
         el.dataset.charsWrapped = "1";
-
         // 子ノードを走査して、テキストノードだけを分解
         const nodes = Array.from(el.childNodes);
         const frag = document.createDocumentFragment();
-
         nodes.forEach((node) => {
             if (node.nodeType === Node.TEXT_NODE) {
             // 改行やスペースも含めて1文字ずつ
@@ -30,14 +26,11 @@ document.addEventListener("DOMContentLoaded", () => {
             frag.appendChild(node);
             }
         });
-
       el.textContent = "";
       el.appendChild(frag);
-
       return Array.from(el.querySelectorAll(".char"));
     };
 //Header, Firstview
-    // const cover = document.querySelector(".js-intro-cover");
     const firstview = document.querySelector(".firstview .bg");
     const logo = document.querySelector(".header .container .js-logo");
     const navItems = document.querySelectorAll(".header .container .inner .pc-nav .pc-sns .js-sns-item");
@@ -54,18 +47,14 @@ document.addEventListener("DOMContentLoaded", () => {
         WebkitMaskImage: "radial-gradient(circle at center, black 0%, transparent 0%)" 
         }
     );
-    // gsap.set(cover, 
-    //     { autoAlpha: 1 }
-    // );
     const pageMainTitle = document.querySelector("#firstview .js-page-main-title");
     const pageSubTitle  = document.querySelector("#firstview .js-page-sub-title");
     const mainChars = wrapChars(pageMainTitle);
     const subChars  = wrapChars(pageSubTitle);
     //Main Title Animation Set
-    gsap.set([...mainChars, ...subChars], {
+    gsap.set([mainChars, subChars], {
         autoAlpha: 0,
         y: 30,
-        // x: -30,
         rotationX: -45,
         filter: "blur(20px)",
         transformPerspective: 600,
@@ -76,7 +65,7 @@ document.addEventListener("DOMContentLoaded", () => {
     firstviewTl.to(
         firstview, {
             WebkitMaskImage: "radial-gradient(circle at center, black 0%, black 100%, transparent 110%)" ,
-            duration: 0.8,
+            duration: 1,
             ease: "power2.in",
         }
     ).to(
@@ -98,7 +87,6 @@ document.addEventListener("DOMContentLoaded", () => {
     ).to(
         mainChars, {
             autoAlpha: 1,
-            // x: 0,
             y: 0,
             rotationX: 0,
             filter: "blur(0px)",
@@ -109,7 +97,6 @@ document.addEventListener("DOMContentLoaded", () => {
     ).to(
         subChars, {
             autoAlpha: 1,
-            // x: 0,
             y: 0,
             rotationX: 0,
             filter: "blur(0px)",
@@ -151,7 +138,6 @@ document.addEventListener("DOMContentLoaded", () => {
                 trigger: "#problems .container .js-text-wrap",
                 start: "top 90%",
                 end: "bottom 30%",
-                // toggleActions: "play reverse play reverse"
             },
             visibility: "hidden",
             duration: 0.3,
@@ -174,7 +160,6 @@ document.addEventListener("DOMContentLoaded", () => {
         scrollTrigger: {
             trigger: resolve,
             start: "top 60%",
-            
         },
         maskSize: "100% 100%",
         WebkitMaskSize: "100% 100%",
@@ -290,19 +275,18 @@ document.addEventListener("DOMContentLoaded", () => {
         scrollTrigger: {
             trigger: featureTitle,
             start: "top 80%",
-            // markers: true,
         },
     });
     featureTitleTl
     .to(featureTitleChars, {
         scaleY: 1,
-        duration: 1,
+        duration: 0.6,
         ease: "back.out(4)",
         stagger: 0.09,
     })
     .to(featureTitle, {
         "--pseudo-opacity": 1,
-        duration: 1,
+        duration: 0.6,
     });
     //セクションコンテンツ
     gsap.utils.toArray([cardsContainer, "#feature .container .js-feature-bottom"])
@@ -323,54 +307,276 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     
 //Program Section
+    //First Section
     const programTitle = document.querySelector("#program .container .catch .js-program-title");
     const programTitleChars = wrapChars(programTitle);
     gsap.set(programTitleChars, {
+        display: "inline-block",
         x: "14rem",
         autoAlpha: 0,
         filter: "blur(20px)",
         willChange: "transform",
     });
-    gsap.set("#program .container", {
-        xPercent: 100,
+    gsap.set("#program .js-container", {
+        backgroundColor: "transparent"
+    });
+    gsap.set([
+        "#program .container .inner .js-intro",
+        "#program .container .inner .js-box-container",
+    ],{ xPercent: 50,
         autoAlpha: 0,
         filter: "blur(20px)",
-        willChange: "transform",
-    })
+        willChange: "transform"
+    });
     
     const programTitleTl = gsap.timeline({
         scrollTrigger: {
             trigger: programTitle,
             start: "top 80%",
-            markers: true,
-            toggleActions: "play reverse play reverse"
         }
     });
     programTitleTl
-    .to("#program .container", {
-        xPercent: 0,
-        duration: 1,
-        ease: "power4.out"
+    .to("#program .js-container", {
+        backgroundColor: "#b3c5db",
+        duration: 2,
     })
-    .to("#program .container", {
-        autoAlpha: 1,
-        filter: "blur(0px)",
-        duration: 1,
-    }, "-=0.5")
     .to(programTitleChars, {
         x: 0,
         autoAlpha: 1,
         filter: "blur(0px)",
-        duration: 1,
-        stagger: 0.06,
-        ease: "power4.out"
-    }, "-=0.2")
+        duration: 0.6,
+        stagger: 0.05,
+        ease: "power3.out"
+    }, "<")
+    .from("#program .container .catch .js-program-sub-title", {
+        y: "1rem",
+        autoAlpha: 0,
+        filter: "blur(20px)",
+        duration: 0.6
+    }, ">-0.3")
     .to(programTitle, {
         "--pseudo-opacity": 1,
         duration: 1,
-    }, ">")
+    }, "<")
+    .to("#program .container .inner .js-intro", {
+        xPercent: 0,
+        autoAlpha: 1,
+        filter: "blur(0px)",
+        ease: "power3.out",
+        duration: 0.6,
+    }, "-=1")
+    .to("#program .container .inner .js-box-container", {
+        xPercent: 0,
+        autoAlpha: 1,
+        filter: "blur(0px)",
+        ease: "power3.out",
+        duration: 0.6,
+    }, "-=0.8");
+
+    //Second Section
+    const linePath__program = document.querySelector("#program .pc.js-line-svg .js-path");
+    const linePath__program__sp = document.querySelector("#program .sp.js-line-svg .js-path");
+
+    //ライン - pc
+    if(!linePath__program) return;
+    if(linePath__program) {
+        const pathLength__program = linePath__program.getTotalLength();
+        gsap.set(linePath__program, {
+            strokeDasharray:  pathLength__program,
+            strokeDashoffset: pathLength__program
+        });
+        gsap.to(linePath__program, {
+            scrollTrigger: { 
+                trigger: "#program .domains",
+                start: "top 55%",
+                end: "bottom 40%",
+                scrub: 1,
+            },
+            strokeDashoffset: 0,
+        });
+    }
+    //ライン - sp
+    if(!linePath__program__sp) return;
+    if(linePath__program__sp) {
+        const pathLength__program__sp = linePath__program__sp.getTotalLength();
+        gsap.set(linePath__program__sp, {
+            strokeDasharray: pathLength__program__sp,
+            strokeDashoffset: pathLength__program__sp,
+        });
+        gsap.to(linePath__program__sp, {
+            scrollTrigger: {
+                trigger: "#program .domains",
+                start: "top 40%",
+                end: "bottom 0%",
+                scrub: 1,
+            },
+            strokeDashoffset: 0,
+        })
+    }
+
+    const domainsTl = gsap.timeline({
+        scrollTrigger: {
+            trigger: "#program .domains",
+            start: "top 80%",
+        }
+    });
+    domainsTl
+    .fromTo("#program .domains .js-domains-head", 
+        {y: "2rem", autoAlpha: 0, filter: "blur(20px)"},
+        {y: 0, autoAlpha: 1, filter: "blur(0px)", duration: 0.6},
+    ).fromTo("#program .domains .domain-wrap",
+        { rotation: 720, autoAlpha: 0, filter: "blur(20px)"},
+        { rotation: 0, autoAlpha: 1, filter: "blur(0px)", duration: 1.5, ease: "power4.out" }
+    )
     
+    const objs = gsap.utils.toArray("#program .domains .obj");
+    const paramsForObjs = objs.map((_, i) => ({
+        phase: (i + 1)  * 0.8,
+        radius: 100 - ((i + 1)* 10),
+        speed: 1 + i * 0.1
+    }));
+    const setters = objs.map(obj => 
+        gsap.quickSetter(obj, "y", "px")
+    );
+    let rotationDegree = 0;
+    let radian = 0;
+    gsap.ticker.add(trigonometry)
+    function trigonometry(time) {
+        setters.forEach((setY, i) => {
+            const { phase, radius, speed } = paramsForObjs[i];
+            setY(Math.sin(time * speed * 0.15 * phase) * radius * 0.8);
+        });
+        rotationDegree += 0.5;
+        radian = rotationDegree * (Math.PI / 180);
+        gsap.set(objs, {
+            rotation: Math.cos(radian) * 45,
+            stagger: 1
+        });
+    }
+
+//Voice Section
+    const voiceTl = gsap.timeline({
+        scrollTrigger: {
+            trigger: "#voice .container",
+            start: "top 80%",
+        }
+    });
+    voiceTl
+    .from("#voice .container .js-catch", {
+        y: "3rem",
+        autoAlpha: 0,
+        filter: "blur(20px)",
+        duration: 0.6,
+        ease: "power2.out"
+    })
+    .to("#voice .container .catch .js-voice-title", {
+        "--transform-scale": "scale(1.0)",
+        duration: 0.6,
+        ease: "back.out(4)"
+    })
+    .to("#voice .container .catch .js-voice-title", {
+        "--transform-scale": "scale(1.0) rotate(25deg)",
+        yoyo: true,
+        repeat: -1,
+        repeatDelay: 0.6,
+        ease: "power2.inOut"
+    })
+
+//Faq Section
+    const faqTl = gsap.timeline({
+        scrollTrigger: {
+            trigger: "#faq .container",
+            start: "top 80%",
+        }
+    });
+    faqTl
+    .from("#faq .container .js-catch", {
+        y: "3rem",
+        autoAlpha: 0,
+        filter: "blur(20px)",
+        duration: 0.6,
+        ease: "power2.out"
+    })
+    .to("#faq .container .catch .js-faq-text", {
+        "--transform-scale": "scale(1.0)",
+        duration: 0.6,
+        ease: "back.out(4)"
+    })
+    .to("#faq .container .catch .js-faq-text", {
+        "--transform-scale": "scale(1.0) rotate(-25deg)",
+        yoyo: true,
+        repeat: -1,
+        repeatDelay: 0.6,
+        ease: "power2.inOut"
+    });
+
+//Cta Section
+    const star = document.querySelector("#cta .star");
+    const starPath = document.querySelector(".cta #motionPath-star");
+    gsap.set(star, {
+        motionPath: {
+            path: starPath,
+            align: starPath,
+            alignOrigin: [0.5, 0.5],
+            autoRotate: true,
+            start: 0,
+            end: 1
+        },
+        autoAlpha: 0
+    });
+    const ctaStarTl = gsap.timeline({
+        scrollTrigger: {
+            trigger: "#cta",
+            start: "top 80%",
+        },
+        onComplete: () => {
+            gsap.set(star, { clearProps: "all" });
+            gsap.fromTo(star, {autoAlpha: 0},{autoAlpha: 1})
+        }
+    });
     
+    ctaStarTl
+    .to(star, { autoAlpha: 0.7, duration: 0.5, ease: "power1.out"})
+    .to(star, {
+        motionPath: {
+            path: "#motionPath-star",
+            align: "#motionPath-star",
+            alignOrigin: [0.5, 0.5],
+            autoRotate: true,
+            start: 0.1,
+            end: 0.8
+        },
+        duration: 4.5,
+        ease: "power3.Out"
+    }, "-=0.5")
+    .to(star, { autoAlpha: 0, duration: 0.5, ease: "power1.out" }, "-=1")
+
+    const ctaTl = gsap.timeline({
+        scrollTrigger: {
+            trigger: "#cta",
+            start: "top 80%",
+        },
+    });
+    ctaTl
+    .from("#cta .js-catch", {
+        y: "3rem",
+        autoAlpha: 0,
+        filter: "blur(20px)",
+        duration: 0.6,
+        ease: "power2.out"
+    })
+    .to("#cta .catch .js-cta-text", {
+        "--transform-scale": "scale(1.0)",
+        duration: 0.6,
+        ease: "back.out(4)"
+    })
+    .to("#cta .catch .js-cta-text", {
+        "--transform-scale": "scale(1.0) rotate(20deg)",
+        yoyo: true,
+        repeat: -1,
+        repeatDelay: 0.6,
+        ease: "power2.inOut"
+    });
 });
 
 window.addEventListener("load", () => {
